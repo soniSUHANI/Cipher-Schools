535ae0260ccb2883c5e7c97fd329fc6c876df1cc
soni <114800693+soniSUHANI@users.noreply.github.com>
2024-06-15 10:50:18 +0530

    Add files via upload

---
+++ course2/ToDoScreen.js
@@ -0,0 +1,10 @@
+import { Component } from "react";
+
+class ToDoScreen extends Component{
+    render(){
+        return(
+            <h1 className="ui heading center">To Do List</h1>
+        );
+    }
+}
+export default ToDoScreen;
