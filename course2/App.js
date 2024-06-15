4206a45b33f7a18350fe314ab2d63ed374e29544
soni <114800693+soniSUHANI@users.noreply.github.com>
2024-06-15 10:12:15 +0530

    Add files via upload

--- course2/App.js
+++ course2/App.js
@@ -1,21 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import {Component} from "react";
 
-function App() {
-  const name = "Soni";
-  return (
-    <>
-    <div>
-      <h1 style={{
-        color: "red",
-        textAlign: "center",
-      }}>Hello from {name}!</h1>
-    </div>
-    <div>
-      <p>This is a paragraph.</p>
-    </div>
-    </>
-  );
+class App extends Component{
+  name = "CipherSchools";
+  render(){
+    return(
+      <>
+      <div>
+        <h1>This is {this.name}!</h1>
+      </div>
+      <div>
+        <p>This is the paragraph and the name is: {this.name}</p>
+      </div>
+      </> 
+    );
+  }
 }
 
 export default App;
